6b57d9ac12ffaa66d5e1c537d209151b9727e031
Anmol Mishra <anmolmishra777@gmail.com>
2026-05-07 10:42:35 +0530

    docs(readme): update YCSB workload benchmarking specs [step 11]

--- src/db.rs
+++ src/db.rs
@@ -5,3 +5,5 @@ use std::sync::Arc;
 use parking_lot::RwLock;
 pub struct LSMEngine { memtable: Arc<RwLock<MemTable>>, _wal: Arc<RwLock<Wal>>, _txn_mgr: TxnManager }
 impl LSMEngine { pub fn open(path: &str) -> std::io::Result<Self> { let wal = Wal::open(format!("{}/wal.log", path))?; Ok(Self { memtable: Arc::new(RwLock::new(MemTable::new())), _wal: Arc::new(RwLock::new(wal)), _txn_mgr: TxnManager::new() }) } pub fn put(&self, key: &[u8], value: &[u8]) -> std::io::Result<()> { self.memtable.write().put(key.to_vec(), Some(value.to_vec())); Ok(()) } pub fn get(&self, key: &[u8]) -> Option<Vec<u8>> { self.memtable.read().get(key).flatten() } }
+
+// Incremental development step #11
