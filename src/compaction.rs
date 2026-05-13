014837e4244f6446ece22df527c3d3cf7c1a36ac
Anmol Mishra <anmolmishra777@gmail.com>
2026-05-13 18:04:11 +0530

    docs(readme): update YCSB workload benchmarking specs [step 23]

--- src/compaction.rs
+++ src/compaction.rs
@@ -4,3 +4,5 @@ pub struct Compactor { _max_levels: usize }
 impl Compactor { pub fn new(max_levels: usize) -> Self { Self { _max_levels: max_levels } } pub fn start(self: Arc<Self>) { thread::spawn(move || { loop { thread::sleep(std::time::Duration::from_secs(10)); } }); } }
 
 // Incremental development step #9
+
+// Incremental development step #23
