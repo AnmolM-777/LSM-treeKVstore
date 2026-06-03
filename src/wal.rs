7785459ff63c6dafbe5c87a72bbf59029693a4ac
Anmol Mishra <anmolmishra777@gmail.com>
2026-06-03 20:32:05 +0530

    fix(wal): add CRC32 checksum validation on recovery [step 61]

--- src/wal.rs
+++ src/wal.rs
@@ -12,3 +12,5 @@ impl Wal { pub fn open<P: AsRef<Path>>(path: P) -> IoResult<Self> { let file = O
 // Incremental development step #33
 
 // Incremental development step #47
+
+// Incremental development step #61
