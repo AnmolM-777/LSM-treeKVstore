50009237fdcb7e27350883119aedcfda1c6c7634
Anmol Mishra <anmolmishra777@gmail.com>
2026-06-14 11:04:03 +0530

    refactor(sstable): optimize sparse index binary search lookup [step 80]

--- src/mvcc.rs
+++ src/mvcc.rs
@@ -11,3 +11,5 @@ impl TxnManager { pub fn new() -> Self { Self { next_seq: AtomicU64::new(1) } } 
 // Incremental development step #52
 
 // Incremental development step #66
+
+// Incremental development step #80
